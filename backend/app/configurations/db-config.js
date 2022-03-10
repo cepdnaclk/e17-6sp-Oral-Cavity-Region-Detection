401d22aa7eabf11376f87024623656364d511ec7
SashiniLiyanage <e17190@eng.pdn.ac.lk>
2022-03-10 20:08:56 +0530

    add db-config.js

--- backend/app/configurations/db-config.js
+++ backend/app/configurations/db-config.js
@@ -1,1 +1,18 @@
-// all database configuration comes here 
+// Database configurations
+
+const mongoos = require('mongoose');
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const connectDB = async () =>{
+    try{
+        const connect = await mongoos.connect(process.env.MONGODB_URI)
+        console.log(`Connected to the DB ,(${connect.connection.id})`)
+    }catch(err){
+        console.log(`DB Error: ${err.message}`)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
